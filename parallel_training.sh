73b4894124cc5573c8f2a5862cdcb99f850de255
AntonioScl <antoniosclocchi@gmail.com>
2025-01-22 15:16:22 +0100

    custom config

---
+++ parallel_training.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=md4
+#SBATCH --output=slurm_out/logs_%j.log
+#SBATCH --error=slurm_out/errs_%j.log
+#SBATCH --partition=h100
+#SBATCH --qos=normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:4
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=300g
+#SBATCH --time=72:00:00
+
+module load gcc/13.2.0 tmux/2.9a python/3.11.7 cuda/12.4.1 cudnn/9.2.1.18-12 openmpi/5.0.3-cuda py-mpi4py/4.0.0
+source /home/sclocchi/venvs_kuma/md4_venv/bin/activate
+
+python md4/main.py --config=md4/configs/md4/custom_openwebtext.py --sharded=false --workdir=./expt
